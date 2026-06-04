6629c94cf2bf57d201f02f9d927ee604986fa924
kashfi002 <thekashfi71@gmail.com>
2026-06-04 15:34:07 +0600

    new collection created and updated

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const express=require('express')
 const dotenv=require('dotenv')
+const cors=require('cors')
 const { MongoClient, ServerApiVersion } = require('mongodb');
 dotenv.config()
 const uri=process.env.MONGODB_URI
 
 const app=express()
 const PORT=process.env.PORT
+app.use(cors())
+app.use(express.json())
 
 const client = new MongoClient(uri, {
   serverApi: {
@@ -16,14 +19,24 @@ const client = new MongoClient(uri, {
 });
 async function run() {
   try {
-    // Connect the client to the server	(optional starting in v4.7)
+   
     await client.connect();
-    // Send a ping to confirm a successful connection
+    
+
+    const db=client.db("study-nook")
+    const roomCollection=db.collection("rooms")
+
+    app.post(('/rooms'), async (req, res) =>{
+        console.log(req.body)
+        const roomData=req.body
+        const result=await roomCollection.insertOne(roomData)
+        res.send(result)
+    })
+
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
+    
   }
 }
 run().catch(console.dir);
